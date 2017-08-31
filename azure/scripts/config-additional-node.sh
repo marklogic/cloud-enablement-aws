ac1c1af67c23a272728cc4ca33d77f31cfe5ad72
Matt Sun <sjf.matt@gmail.com>
2017-08-31 14:36:14 -0700

    init.sh

--- azure/scripts/config-additional-node.sh
+++ azure/scripts/config-additional-node.sh
@@ -8,45 +8,14 @@
 #                enable-high-availability bootstrap-node joining-host
 ######################################################################################################
 
-USER=$1
-PASS=$2
-AUTH_MODE=$3
+source ./init.sh $1 $2 $3
+
+# variables
 N_RETRY=$4
 RETRY_INTERVAL=$5
 ENABLE_HA=$6
 BOOTSTRAP_HOST=$7
 JOINING_HOST=$8
-
-# log file to record all the activities
-LOG="/tmp/log-additional-node-$(date +"%Y%m%d%h%m%s").log"
-# Suppress progress meter, but still show errors
-CURL="curl -s -S"
-# add authentication related options, required once security is initialized
-AUTH_CURL="${CURL} --${AUTH_MODE} --user ${USER}:${PASS}"
-
-######################################################################################################
-# restart_check(hostname, baseline_timestamp, caller_lineno)
-#
-# Use the timestamp service to detect a server restart, given a
-# a baseline timestamp.
-# Returns 0 if restart is detected, exits with an error if not.
-######################################################################################################
-
-function restart_check {
-  LAST_START=`$AUTH_CURL "http://$1:8001/admin/v1/timestamp" |& tee -a $LOG`
-  for i in `seq 1 ${N_RETRY}`; do
-    if [ "$2" == "$LAST_START" ] || [ "$LAST_START" == "" ]; then
-      echo "Server didn't restart. Retry in $RETRY_INTERVAL seconds..." >> $LOG
-      sleep ${RETRY_INTERVAL}
-      LAST_START=`$AUTH_CURL "http://$1:8001/admin/v1/timestamp" |& tee -a $LOG`
-    else
-      echo "Server successfully restarted." >> $LOG
-      return 0
-    fi
-  done
-  echo "ERROR: Line $3: Failed to restart $1" >> $LOG
-  exit 1
-}
 
 #####################################################################################################
 #
@@ -54,14 +23,14 @@ function restart_check {
 # 
 #####################################################################################################
 
-echo "Writing data into /etc/marklogic.conf" >> $LOG
+INFO "Writing data into /etc/marklogic.conf"
 echo "export MARKLOGIC_HOSTNAME=$JOINING_HOST" >> /etc/marklogic.conf |& tee -a $LOG
 
-echo "Restarting the server to pick up changes in /etc/marklogic.conf..." >> $LOG
+INFO "Restarting the server to pick up changes in /etc/marklogic.conf"
 /etc/init.d/MarkLogic restart |& tee -a $LOG
 sleep 10
 
-echo "Adding host to cluster: $JOINING_HOST..." >> $LOG
+INFO "Adding host $JOINING_HOST to the cluster $BOOTSTRAP_HOST"
 # initialize MarkLogic Server on the joining host
 TIMESTAMP=`$CURL -X POST -d "" \
    http://${JOINING_HOST}:8001/admin/v1/init \
@@ -69,19 +38,20 @@ TIMESTAMP=`$CURL -X POST -d "" \
    | grep "last-startup" \
    | sed 's%^.*<last-startup.*>\(.*\)</last-startup>.*$%\1%'`
 if [ "$TIMESTAMP" == "" ]; then
-  echo "ERROR: Failed to initialize $JOINING_HOST" >&2 >> $LOG
+  ERROR "Failed to initialize $JOINING_HOST"
   exit 1
 fi
-echo "Checking server restart..." >> $LOG
+
+INFO "Checking server restart"
 restart_check $JOINING_HOST $TIMESTAMP $LINENO
 
 # retrieve the joining host's configuration
-echo "Retrieving the joining host's configuration..." >> $LOG
+INFO "Retrieving the joining host's configuration"
 JOINER_CONFIG=`$CURL -X GET -H "Accept: application/xml" \
     http://${JOINING_HOST}:8001/admin/v1/server-config |& tee -a $LOG`
 echo $JOINER_CONFIG | grep -q "^<host"
 if [ "$?" -ne 0 ]; then
-  echo "ERROR: Failed to fetch server config for $JOINING_HOST" >> $LOG
+  ERROR "Failed to fetch server config for $JOINING_HOST"
   exit 1
 fi
 
@@ -98,11 +68,11 @@ $AUTH_CURL -X POST -o cluster-config.zip -d "group=Default" \
       -H "Content-type: application/x-www-form-urlencoded" \
       http://${BOOTSTRAP_HOST}:8001/admin/v1/cluster-config |& tee -a $LOG
 if [ "$?" -ne 0 ]; then
-  echo "ERROR: Failed to fetch cluster config from $BOOTSTRAP_HOST" >> $LOG
+  ERROR "Failed to fetch cluster config from $BOOTSTRAP_HOST"
   exit 1
 fi
 if [ `file cluster-config.zip | grep -cvi "zip archive data"` -eq 1 ]; then
-  echo "ERROR: Failed to fetch cluster config from $BOOTSTRAP_HOST" >> $LOG
+  ERROR "Failed to fetch cluster config from $BOOTSTRAP_HOST"
   exit 1
 fi
 
@@ -113,19 +83,19 @@ fi
 #
 #####################################################################################################  
 
-echo "Sending the cluster config data to the joining host..." >> $LOG
+INFO "Sending the cluster config data to the joining host"
 TIMESTAMP=`$CURL -X POST -H "Content-type: application/zip" \
     --data-binary @./cluster-config.zip \
     http://${JOINING_HOST}:8001/admin/v1/cluster-config \
     |& tee -a $LOG \
     | grep "last-startup" \
     | sed 's%^.*<last-startup.*>\(.*\)</last-startup>.*$%\1%'`
-echo "Checking server restart..." >> $LOG
+INFO "Checking server restart"
 restart_check $JOINING_HOST $TIMESTAMP $LINENO
 rm ./cluster-config.zip
-echo "...$JOINING_HOST successfully added to the cluster." >> $LOG
+INFO "$JOINING_HOST successfully added to the cluster"
 
 if [ "$ENABLE_HA" == "True" ]; then
-  echo "Configurating high availability on the cluster..." |& tee -a $LOG
-  . ./high-availability.sh $USER $PASS $AUTH_MODE $BOOTSTRAP_HOST |& tee -a $LOG
+  INFO "Configurating high availability on the cluster"
+  . ./high-availability.sh $USER $PASS $AUTH_MODE $BOOTSTRAP_HOST
 fi
--- azure/scripts/config-bootstrap-node.sh
+++ azure/scripts/config-bootstrap-node.sh
@@ -6,44 +6,13 @@
 #                n-retry retry-interval hostname
 ######################################################################################################
 
+source ./init.sh $1 $2 $3
+
 # variables
-USER=$1
-PASS=$2
-AUTH_MODE=$3
 SEC_REALM=$4
 N_RETRY=$5
 RETRY_INTERVAL=$6
 BOOTSTRAP_HOST=$7
-
-# log file to record all the activities
-LOG="/tmp/init-bootstrap-node-$(date +"%Y%m%d%h%m%s").log"
-# Suppress progress meter, but still show errors
-CURL="curl -s -S"
-# add authentication related options, required once security is initialized
-AUTH_CURL="${CURL} --${AUTH_MODE} --user ${USER}:${PASS}"
-
-######################################################################################################
-# restart_check(hostname, baseline_timestamp, caller_lineno)
-#
-# Use the timestamp service to detect a server restart, given a baseline timestamp. 
-# Returns 0 if restart is detected, exits with an error if not.
-######################################################################################################
-
-function restart_check {
-  LAST_START=`$AUTH_CURL "http://$1:8001/admin/v1/timestamp" |& tee -a $LOG`
-  for i in `seq 1 ${N_RETRY}`; do
-    if [ "$2" == "$LAST_START" ] || [ "$LAST_START" == "" ]; then
-      echo "Server didn't restart. Retry in $RETRY_INTERVAL seconds..." >> $LOG
-      sleep ${RETRY_INTERVAL}
-	  LAST_START=`$AUTH_CURL "http://$1:8001/admin/v1/timestamp" |& tee -a $LOG`
-    else
-      echo "Server successfully restarted." >> $LOG
-      return 0
-    fi
-  done
-  echo "ERROR: Line $3: Failed to restart $1" >> $LOG
-  exit 1
-}
 
 ######################################################################################################
 # Bring up the first host in the cluster. The following
@@ -53,18 +22,18 @@ function restart_check {
 # GET /admin/v1/timestamp is used to confirm restarts.
 ######################################################################################################
 
-echo "Writing data into /etc/marklogic.conf..." >> $LOG
+INFO "Writing data into /etc/marklogic.conf"
 echo "export MARKLOGIC_HOSTNAME=$BOOTSTRAP_HOST" >> /etc/marklogic.conf |& tee -a $LOG
 
-echo "Restarting the server to pick up changes in /etc/marklogic.conf..." >> $LOG
+INFO "Restarting the server to pick up changes in /etc/marklogic.conf"
 /etc/init.d/MarkLogic restart |& tee -a $LOG
 sleep 10
 
-echo "Initializing $BOOTSTRAP_HOST..." >> $LOG
+INFO "Initializing $BOOTSTRAP_HOST"
 $CURL -X POST -d "" http://${BOOTSTRAP_HOST}:8001/admin/v1/init &>> $LOG
 sleep 10
 
-echo "Initializing database admin and security database..." >> $LOG
+INFO "Initializing database admin and security database"
 TIMESTAMP=`$CURL -X POST \
    -H "Content-type: application/x-www-form-urlencoded" \
    --data "admin-username=${USER}" --data "admin-password=${PASS}" \
@@ -74,13 +43,13 @@ TIMESTAMP=`$CURL -X POST \
    | grep "last-startup" \
    | sed 's%^.*<last-startup.*>\(.*\)</last-startup>.*$%\1%'`
 if [ "$TIMESTAMP" == "" ]; then
-  echo "ERROR: Failed to get instance-admin timestamp." >&2 >> $LOG
+  ERROR "Failed to get instance-admin timestamp"
   exit 1
 fi
 
 # Test for successful restart
-echo "Checking server restart..." >> $LOG
+INFO "Checking server restart"
 restart_check $BOOTSTRAP_HOST $TIMESTAMP $LINENO
 
-echo "Initialization complete for $BOOTSTRAP_HOST..." >> $LOG
+INFO "Initialization complete for $BOOTSTRAP_HOST"
 exit 0
