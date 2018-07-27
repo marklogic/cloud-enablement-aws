abf887d4fc2b1a8f0a2950ea94724aa60e108022
Hongyang Li <hli@hli-z640.marklogic.com>
2018-07-27 10:09:33 -0700

    Changed last modified time to the time building the package

--- aws/lambda/package/managedeni_pkg.sh
+++ aws/lambda/package/managedeni_pkg.sh
@@ -3,17 +3,7 @@
 [ -e ../managed_eni.zip ] && rm ../managed_eni.zip
 [ -e ../version.txt ] && rm ../version.txt
 
-#get the last modified time of the files
-LAST_MOD_TIME_UNIX=0
-LAST_MOD_TIME=0
-for f in `find .. -type f`; do
-    TMP=`date -r $f +%s`
-    if [[ $TMP -gt $LAST_MOD_TIME_UNIX ]]; then
-        LAST_MOD_TIME_UNIX=$TMP
-        LAST_MOD_TIME=`date -r $f`
-    fi
-done
-echo $LAST_MOD_TIME > ../version.txt
+echo `date` > ../version.txt
 
 cp custom_resource_base.zip ../managed_eni.zip
 
--- aws/lambda/package/nodemanager_pkg.sh
+++ aws/lambda/package/nodemanager_pkg.sh
@@ -2,17 +2,7 @@
 [ -e ../node_manager.zip ] && rm ../node_manager.zip
 [ -e ../version.txt ] && rm ../version.txt
 
-#get the last modified time of the files
-LAST_MOD_TIME_UNIX=0
-LAST_MOD_TIME=0
-for f in `find .. -type f`; do
-    TMP=`date -r $f +%s`
-    if [[ $TMP -gt $LAST_MOD_TIME_UNIX ]]; then
-        LAST_MOD_TIME_UNIX=$TMP
-        LAST_MOD_TIME=`date -r $f`
-    fi
-done
-echo $LAST_MOD_TIME > ../version.txt
+echo `date` > ../version.txt
 
 cd ..
 zip node_manager.zip nodemanager.py utils.py version.txt
